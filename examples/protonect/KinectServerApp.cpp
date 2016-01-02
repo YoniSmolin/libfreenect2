465400184517cefb7a49935269e8d66078750a37
Arik <Yoni.Smolin@gmail.com>
2016-01-02 14:48:47 +0200

    Tidy KinectServerApp.cpp

--- examples/protonect/KinectServerApp.cpp
+++ examples/protonect/KinectServerApp.cpp
@@ -70,22 +70,9 @@ int main(int argc, char *argv[])
 	server.WaitForClient();
 	protonect_shutdown = !server.SendMetadata(properties->Type);
 
-	long            ms; // Milliseconds
-	time_t          s;  // Seconds
-	struct timespec spec;
-
-
-
 	// main loop
 	while(!protonect_shutdown)
 	{
-		clock_gettime(CLOCK_REALTIME, &spec);
-
-		s  = spec.tv_sec;
-		ms = round(spec.tv_nsec / 1.0e6); // Convert nanoseconds to milliseconds
-
-		printf("Current time: %"PRIdMAX".%03ld seconds since the Epoch\n",(intmax_t)s, ms);
-
 		timer.FrameStart();
 		
 		// obtain frame from libfreenect runtime - 4 bytes per pixel 
